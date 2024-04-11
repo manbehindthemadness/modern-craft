959e6335f461a5c84c2f93b85a4a5409c2f80015
Kev0 <kev0@me.com>
2024-04-11 18:27:11 -0400

    Updated name... again.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open("README.md", "r") as fh:
 
 
 setup(
-    name='modern-craft',
+    name='mcraft',
     version='0.0.1',
     packages=find_packages(),
     install_requires=install_requires,
