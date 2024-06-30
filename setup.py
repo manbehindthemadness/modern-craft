3e732cae1e85d29fec8a13b580aa90decbc20709
Kevin <manbehindthemadness@gmail.com>
2024-06-30 18:30:58 +0200

    Incremented version.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mcraft',
-    version='0.0.3',
+    version='0.0.4a',
     packages=find_packages(),
     install_requires=install_requires,
     entry_points={
